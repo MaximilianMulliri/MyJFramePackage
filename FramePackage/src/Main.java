bf3df6d44f9b165a0b372abb00d2645b007aae4e
userx <maximilianmulliri@outlook.de>
2023-01-16 13:30:09 +0100

    added MyInterface

--- FramePackage/src/Main.java
+++ FramePackage/src/Main.java
@@ -6,11 +6,18 @@ public class Main  {
 	
 	
 	public static void main(String[] args) {
+		String name = "Max";
+		MyInterface interFace = (x) ->{
+		System.out.println(x);
+		};
 
 		MyJButton testBtn = new MyJButton(0,0,"Test");
 		
-	
+		testBtn.addActionListener((x) -> {
 			
+		System.out.println("pew puw");
+		System.out.println("Hello "+x);
+		});
 		
 			
 		MyJPanel panel = new MyJPanel(0,0,404,210);				//Memo: gap between Frame and JPanel = 16
@@ -18,6 +25,7 @@ public class Main  {
 		MyJFrame mainFrame = new MyJFrame(0,0,420,420,"MainFrame");
 		
 		mainFrame.add(panel);
+		interFace.message(name);
 		panel.add(testBtn);
 	
 	}
